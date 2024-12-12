f9a5af66acfe94a2a7abbd5b01d7f868c8aae72f
Agasthya-Samyak-Jnan <agasthyasamyakjnan5577@gmail.com>
2024-12-12 20:52:29 +0530

    Solved Few Problems.

---
+++ Database/Duplicate Emails.sql
@@ -0,0 +1,9 @@
+-- SQL Schema for the Question
+Create table If Not Exists Person (id int, email varchar(255))
+Truncate table Person
+insert into Person (id, email) values ('1', 'a@b.com')
+insert into Person (id, email) values ('2', 'c@d.com')
+insert into Person (id, email) values ('3', 'a@b.com')
+
+/* Write your MySQL query statement below */
+select email from Person group by email having COUNT(*) > 1;
