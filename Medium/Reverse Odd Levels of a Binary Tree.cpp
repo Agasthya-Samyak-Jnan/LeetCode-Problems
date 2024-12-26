69c70a6064b7b7133a808c7d26e1adfbaea06f8a
Agasthya-Samyak-Jnan <agasthyasamyakjnan5577@gmail.com>
2024-12-26 14:57:48 +0530

    Solved Few Problems.

--- Medium/Reverse Odd Levels of a Binary Tree.cpp
+++ Medium/Reverse Odd Levels of a Binary Tree.cpp
@@ -17,31 +17,31 @@ struct TreeNode {
 /* BREADTH-FIRST SEARCH */
 class Solution {
 public:
-    void Reverse_Level (queue<TreeNode*>& level) {
+    void Reverse_Level (vector<TreeNode*>& level) {
         int n = level.size();
-        for (int i=0;i<n/2;i++) { swap(level.front()->val,level.back()->val); }
+        for (int i=0;i<n/2;i++) { swap(level[i]->val,level[n-i-1]->val); }
     }
      
     TreeNode* reverseOddLevels(TreeNode* root) {
         
         queue<TreeNode*> q;
+        vector<TreeNode*> level;
         TreeNode* ROOT = root;
-        int curr_level = 1;
+        int curr_level = 0;
         
         q.push(root);
         
         while (!q.empty()) {
-            
             int l = q.size();
             for (int i=0;i<l;i++) {
                 root = q.front(); q.pop();
+                level.push_back(root);
                 if (root->left) { q.push(root->left); }
                 if (root->right) { q.push(root->right); }
             }
-            
-            if (curr_level % 2 == 1) { Reverse_Level(q); }
+            if (curr_level % 2 == 1) { Reverse_Level(level); }
+            level = {};
             curr_level++;
-
         }
         
         return ROOT;
